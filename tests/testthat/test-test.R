797be0c04dd6950cc53472b2f5f5e70a578b65e3
Candace Savonen <cansav09@gmail.com>
2023-10-10 14:30:27 -0700

    Fix logic for realz

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,16 +1,16 @@
 test_that("githubr", {
 
-  git_pat <- try(gitcreds::gitcreds_get()$password, silent = TRUE)[1]
+  git_pat <- try(gitcreds::gitcreds_get(), silent = TRUE)
 
   # Only is tested if we have credentials available
-  if (class(git_pat) == "character") {
+  if (class(git_pat) == "gitcreds") {
 
     # Test the issue getter
     issues_df <- get_issues("jhudsl/OTTR_Template")
     testthat::expect_true(is.data.frame(issues_df))
 
     # Test the issue getter with a PAT
-    issues_df <- get_issues("jhudsl/OTTR_Template", git_pat = git_pat)
+    issues_df <- get_issues("jhudsl/OTTR_Template", git_pat = git_pat$password)
     testthat::expect_true(is.data.frame(issues_df))
 
     # Test the repo getter
@@ -18,7 +18,7 @@ test_that("githubr", {
     testthat::expect_true(is.data.frame(repos_df))
 
     # Test the repo getter
-    repos_df <- get_repos("fhdsl", git_pat = git_pat)
+    repos_df <- get_repos("fhdsl", git_pat = git_pat$password)
     testthat::expect_true(is.data.frame(repos_df))
   }
 
