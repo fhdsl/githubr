0bb7127c18b92f6014606b9194ac1fefd51c24de
Candace Savonen <cansav09@gmail.com>
2023-10-18 09:48:38 -0400

    Add an extra check step for the git pat

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,24 +1,22 @@
 test_that("githubr", {
 
-  git_pat <- try(gitcreds::gitcreds_get(), silent = TRUE)
+  git_credentials <- try(gitcreds::gitcreds_get()$password, silent = TRUE)
+
+  git_pat_found <- grepl("ghp", git_credentials)
 
   # Only is tested if we have credentials available
-  if (class(git_pat) == "gitcreds") {
+  if (git_pat_found) {
 
     # Test the issue getter
     issues_df <- get_issues("jhudsl/OTTR_Template")
     testthat::expect_true(is.data.frame(issues_df))
 
     # Test the issue getter with a PAT
-    issues_df <- get_issues("jhudsl/OTTR_Template", git_pat = git_pat$password)
+    issues_df <- get_issues("jhudsl/OTTR_Template", git_pat = git_credentials)
     testthat::expect_true(is.data.frame(issues_df))
 
     # Test the repo getter
-    repos_df <- get_repos("fhdsl")
-    testthat::expect_true(is.data.frame(repos_df))
-
-    # Test the repo getter
-    repos_df <- get_repos("fhdsl", git_pat = git_pat$password)
+    repos_df <- get_repos("fhdsl", git_pat = git_credentials)
     testthat::expect_true(is.data.frame(repos_df))
   }
 
